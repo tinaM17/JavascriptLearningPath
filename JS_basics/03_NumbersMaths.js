35679a7f0285e3cea7abdc7b97e5334af4bf3ab2
Tina Majumder <tinamajumder@Tinas-Air.lan>
2023-11-18 18:22:12 +0530

    update number file

--- JS_basics/03_NumbersMaths.js
+++ JS_basics/03_NumbersMaths.js
@@ -1,31 +1,31 @@
 // **********  Numbers and Math *************
 
-// const balance=100000;
-// const score= new Number(500.768)
+const balance=100000;
+const score= new Number(500.768)
 
-// console.log( typeof balance);
-// console.log(typeof score);
+console.log( typeof balance); // Number
+console.log(typeof score); //Object
 
 // //toString()
-// console.log(balance.toString().length);
+// console.log(balance.toString().length); //6
 
 // //toFixed()
-// console.log(balance.toFixed(2));
+//console.log(balance.toFixed(2)); //100000.00
 
 // //toPrecision()
-// console.log(score.toPrecision(4));
+//console.log(score.toPrecision(4)); //500.8
 
 // //toLocaleString()
-// console.log(balance.toLocaleString('en-In'));
+// console.log(balance.toLocaleString('en-In')); // 1,00,000
 
 // *************** Maths ********************
 // console.log(Math.abs(-45));
-// console.log(Math.round(4.6)); //round off the number
+// console.log(Math.round(4.5)); //round off the number, output=5
 // console.log(Math.ceil(4.2)); //ceiling means it returns the next value here returns 5
-// console.log(Math.floor(4.7)); // it returns the previous value here returns 4
+// console.log(Math.floor(4.8)); // it returns the previous value here returns 4
 // console.log(Math.max(2,8,4,1,0,6));
 // console.log(Math.min(2,8,4,1,0,6));
-// console.log(Math.pow(2,3));
+// console.log(Math.pow(2,3)); //ouput:- 8
 // console.log(Math.sqrt(16));
 
 // console.log(Math.random()); //generates decimal numbers btw 0 to 1
